a347ff114ced36b76dc6787e2719bbca26364396
ariyoeyitayo07@gmail.com <ariyoeyitayo07@gmail.com>
2024-09-11 19:05:19 +0100

    Fixed the auth

--- app/join/page.js
+++ app/join/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import React from 'react'
 import google from './google.png'
 import Link from 'next/link'
-import Router from 'next/navigation'
+import { useRouter } from 'next/router'
 import { supabase } from '@/lib/supabaseClient'
 const Joinpage = () => {
 
@@ -19,21 +19,22 @@ const Joinpage = () => {
             } else {
                 console.log(`Logged in succesfully: ${data}`);
             };
+            
      };
   return (
     <div className='bg-white h-screen'>
-        <div className='mx-[41rem] flex md:pt-28'>
+        <div className='md:mx-[41rem] mx-44 pt-60 flex md:pt-28'>
             <Logo />
         </div>
         <div className='md:mx-[22rem] text-center'>
-            <h2 className='text-[2.6rem] text-black font-normal p-6'>Save the right way, <br /> Use <span className='text-[#06D6A0] font-medium'>SavePal</span></h2>
+            <h2 className='md:text-[2.6rem] text-3xl text-black font-normal p-6'>Save the right way, <br /> Use <span className='text-[#06D6A0] font-medium'>SavePal</span></h2>
         </div>
         <div className='mx-auto'>
-            <button onClick={GoogleLogin} className='bg-[#D5DCF9] mx-[5vw] lg:mx-[35vw] flex p-2 rounded-full w-96 border border-gray-400'>
+            <button onClick={GoogleLogin} className='bg-[#D5DCF9] mx-auto lg:mx-[35vw] flex p-2 rounded-full md:w-96 w-[20rem] border border-gray-400'>
                 <Image alt='google-img' className='h-7 w-7 mx-3' src={google} />
                 <h4 className='text-black text-[19px] font-medium px-1'>Continue with google.</h4>
             </button>
-            <p className='text-black mx-auto w-96 py-3'>By clicking continue you agree to the <Link href="t&c" className='text-blue-700'>terms and conditions</Link> and <Link href="privacy-policy" className='text-blue-700'>privacy policy</Link> of SavePal.</p>
+            <p className='text-black mx-auto w-96 py-3 md:px-0 px-5'>By clicking continue you agree to the <Link href="t&c" className='text-blue-700'>terms and conditions</Link> and <Link href="privacy-policy" className='text-blue-700'>privacy policy</Link> of SavePal.</p>
         </div>
     </div>
   )
